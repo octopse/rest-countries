4b5a27edebc159ad10a4883cb1b483c3caecb82d
Octopse <octopse.octopse@gmail.com>
2022-10-28 18:54:51 +0200

    Added nunito font and removed unnecessary code

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nunito: "nunito, sans-serif",
+      }
+    },
   },
   plugins: [],
 }
