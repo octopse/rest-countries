3056a2818c81603f57daee2951bc3f20b5732419
Octopse <octopse.octopse@gmail.com>
2022-10-29 08:32:17 +0200

    Added country card component

---
+++ components/CountryCard.jsx
@@ -0,0 +1,19 @@
+
+function CountryCard({country}){
+  return(
+    <div className="mt-12 bg-lmrs-element text-lmrs-text shadow rounded-md overflow-hidden">
+      <img src={country.flags.png} alt={country.name + "flag"}></img>
+      <div className="text-sm p-5 space-y-1 font-semibold">
+        <h3 className="text-lg font-extrabold mb-2">{country.name.common}</h3>
+        <p>Population: <span className="font-light">{numberWithCommas(country.population)}</span></p>
+        <p>Region: <span className="font-light">{country.region}</span></p>
+        <p>Capital: <span className="font-light">{country.capital}</span></p>
+      </div>
+    </div>
+  )
+}
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+export default CountryCard
