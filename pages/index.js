0784bc5aa855835d87e1edb4259298803ec99d1c
Octopse <octopse.octopse@gmail.com>
2022-10-29 09:23:22 +0200

    Added components to the page to display

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import Head from 'next/head'
 
 import Search from '../components/Search'
 import Filter from '../components/Filter'
+import Countries from '../components/Countries'
 
-export default function Home() {
+export default function Home({countries}) {
   return (
     <div className="bg-lmrs-back font-nunito">
       <Head>
@@ -15,11 +16,24 @@ export default function Home() {
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;600;800&display=swap" rel="stylesheet" /> 
       </Head>
       <main className="mt-12 w-4/5 mx-auto">
-        <div className="flex justify-between">
+        <section className="flex justify-between">
           <Search />
           <Filter />
-        </div>
+        </section>
+        <section>
+          <Countries countries={countries} />
+        </section>
+
       </main>
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const response = await fetch(`https://restcountries.com/v3.1/all`)
+  const countries = await response.json()
+
+  return {
+    props: {countries}
+  }
 }
