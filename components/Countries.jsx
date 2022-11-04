ae64dc3bc0ff1100c2c756ed14a992d91ec8c961
Octopse <octopse.octopse@gmail.com>
2022-11-04 03:58:35 +0100

    fixed: last row or on search having weird card display

--- components/Countries.jsx
+++ components/Countries.jsx
@@ -4,7 +4,8 @@ import CountryCard from './CountryCard'
 function Countries({countries}) {
   return (
     <div className="flex flex-col w-full mx-auto space-y-8
-      xsm:flex-row xsm:flex-wrap xsm:justify-between">
+      xsm:flex-row xsm:flex-wrap xsm:gap-x-[5%]
+      lg:gap-x-[2.5%]">
       {countries.map((country) => (
         <CountryCard key={country.cca2} country={country}/>
       ))}
