6faf044aa2eb35247dc0ffc87dee7a28af8be1c4
Octopse <octopse.octopse@gmail.com>
2022-11-04 16:35:29 +0100

    feat: Added progress bar to all pages

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,10 @@
 import Nav from './Nav'
+import NextNProgress from 'nextjs-progressbar'
 
 function Layout ({children}){
   return(
     <div className="font-nunito bg-lmrs-back min-h-screen">
+      <NextNProgress color="#111517"/>
       <Nav />
       {children}
     </div>
