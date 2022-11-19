5a8519d72f0838e29856cc1b91999574fab7ad21
Octopse <octopse.octopse@gmail.com>
2022-11-19 04:31:27 +0100

    fix: mobile navbar items touching each other

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,13 +7,13 @@ function Nav (){
   return(
     <header className="bg-lmrs-element w-full flex justify-center shadow dark:bg-dmrs-element">
       <nav className="w-4/5 max-w-[1440px] flex justify-between items-center py-4">
-        <h1 className="text-xl font-semibold ">Where in the world ?</h1>
+        <h1 className="text-xl font-semibold ">Where in the world?</h1>
         <div className="flex items-center cursor-pointer ">
           {theme === 'light' ? 
             <div onClick={() => setTheme('dark')} className="flex">
               <IoIosSunny className='mr-2 text-xl'/><span>Light Mode</span>
             </div> 
-            : <div  onClick={() => setTheme('light')} className="flex">
+            : <div  onClick={() => setTheme('light')} className="flex items-center ml-6">
               <IoMoon className='mr-2 text-xl'/><span>Dark Mode</span>
             </div>
           }
